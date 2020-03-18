cf06203f4027a6b2395fedebcc21aeded5650ada
Shlok Mehrotra <shlokmehrotra@gmail.com>
2020-03-17 20:58:57 -0700

    init bot instance

---
+++ server.py
@@ -0,0 +1,12 @@
+import discord
+import csv
+from discord.ext import commands
+
+
+client = discord.Client()
+bot = commands.Bot("")
+infoarr = []
+
+
+client.run('Njg5NjgwOTY2ODMwMzkxMzA0.XnGbuw.7CvKq0ixc1fsIx2-9LpHHt9LG-A')
+
