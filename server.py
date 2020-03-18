9f458147b757ae9b3de921efb170b275a6a35bd1
Shlok Mehrotra <shlokmehrotra@gmail.com>
2020-03-17 21:16:31 -0700

    bot online

--- server.py
+++ server.py
@@ -6,7 +6,18 @@ from discord.ext import commands
 client = discord.Client()
 bot = commands.Bot("")
 infoarr = []
+political_events = ["politic", "yang", "hillary", "trump", "liberal", "libtard", "leftist", "rightist", "tulsi", "democrats", "yengu", "yeng", "poll", "veto", "ynag", "y4ng"]
+memes = ["instagram", "reddit"]
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    guilds = client.guilds
 
 
-client.run('Njg5NjgwOTY2ODMwMzkxMzA0.XnGbuw.7CvKq0ixc1fsIx2-9LpHHt9LG-A')
 
+
+
+
+#dont touch the lines below
+
+client.run("Njg5NjgwOTY2ODMwMzkxMzA0.XnGfew.zTwns19a74uIQYqRKQZ8DHE6Xj0")
