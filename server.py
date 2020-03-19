62bcbf9f1b200d689ba30e1da595dc2292f5a944
Shubham Kumar <kumar.shubham5504@gmail.com>
2020-03-18 17:37:43 -0700

    :popcorn: prenk

--- server.py
+++ server.py
@@ -1,30 +1,57 @@
 import discord
-import csv
+import numpy as np
+import pandas as pd
 from discord.ext import commands
+import time
 
+client = commands.Bot(command_prefix = "!")
+tasks = pd.DataFrame(columns = ["author", "task", "time"])
 
-client = discord.Client()
-bot = commands.Bot("")
-infoarr = []
-political_events = ["politic", "yang", "hillary", "trump", "liberal", "libtard", "leftist", "rightist", "tulsi", "democrats", "yengu", "yeng", "poll", "veto", "ynag", "y4ng"]
-memes = ["instagram", "reddit"]
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-    guilds = client.guilds
-@client.event
-async def on_message(message):
-	#message.content -> to extract the content of the message
-	#message.author -> who wrote the message
-	#await message.channel.send("the message you want to send") -> how to send a message to the channel
-	if(message.content == "prenk"):
-		await message.channel.send("prenk")
-	#here do whatever you want with the new message
+  print('We have logged in as {0.user}'.format(client))
+  guilds = client.guilds
 
+@client.command()
+async def ping(ctx):
+  await ctx.send(f'Chong from Wuhan! {round(client.latency * 1000)}ms')
 
+# time format DD:HH:MM
+# add items to schedule
+# time_fromnow takes up DD:HH:MM format
+def time_waste(time_from_now):
+  print(time.ctime())
 
+@client.command()
+async def add(ctx, task, time):
+  global tasks
+  # check for duplicate tasks
+  if task not in list(tasks['task']):
+    tasks = tasks.append(pd.Series([ctx.author, task, time], index=tasks.columns), ignore_index=True)
+    print(tasks)
+    await ctx.send(f"Added {task} successfully! :rotating_light:")
+  else:
+    await ctx.send(f"{task} already exists. Did you want to update it? :eggplant:")
 
+# delete items from schedule
+@client.command()
+async def delete(ctx, task):
+  await ctx.send("Delete" ,  ctx.content, task.content)
 
-#dont touch the lines below
+# update items
+@client.command()
+async def update(ctx, task, time):
+  await ctx.send("Update")
 
+# show items specific to user
+@client.command()
+async def show(ctx):
+  await ctx.send("Show")
+
+# indicate that item is completed
+@client.command()
+async def complete(ctx, task):
+  await ctx.send("Complete")
+
+#dont touch the below tings
 client.run("Njg5NzcwODc3OTk0NTMyODg0.XnHtRA.woz3RKnzeaztW2dTrhpbLekA68g")
