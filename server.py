3b95fd64ef67fa3051aad8cda920427619686f00
shlokmehrotra <shlokmehrotra@gmail.com>
2020-04-11 23:48:09 -0700

    new token

--- server.py
+++ server.py
@@ -285,4 +285,4 @@ async def complete(ctx, *, task):
 #dont touch the below tings
 
 
-client.run("Njg5NzcwODc3OTk0NTMyODg0.XnHtRA.woz3RKnzeaztW2dTrhpbLekA68g")
+client.run("Njg5NzcwODc3OTk0NTMyODg0.XpK5Og.Sm3ZYHz2mzAg7bDyrFdCGkf-ss0")
